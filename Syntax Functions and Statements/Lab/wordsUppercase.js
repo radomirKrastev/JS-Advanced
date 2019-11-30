d34e027a9bb5e01e825d4951e3c96e54697c2055
radomirKrastev <radomirkostantinov@gmail.com>
2019-11-30 13:45:07 +0200

    words uppercase

---
+++ Syntax Functions and Statements/Lab/wordsUppercase.js
@@ -0,0 +1,11 @@
+function extract(text){
+
+    let result = text.match(/\w+/mg).map(x=>x.toUpperCase()).join(", ");   
+
+    console.log(result)
+}
+
+extract(`
+    Hi, how are 
+    you?`
+    );
