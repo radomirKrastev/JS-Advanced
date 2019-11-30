70d3b4135abdfbafcc9d8261379efcbe96d13e0e
radomirKrastev <radomirkostantinov@gmail.com>
2019-11-30 20:40:52 +0200

    same numbers

---
+++ Syntax Functions and Statements/Exercise/sameNumbers.js
@@ -0,0 +1,19 @@
+function checkNumbers(x) {
+
+    let equalNumbers = true;
+    let numbers = x.toString().split("");
+
+    for (i = 0; i < numbers.length - 1; i++) {
+
+        if (numbers[i] !== numbers[i + 1]) {
+            equalNumbers = false;
+            break;
+        }
+    }
+
+    sum = numbers.reduce((a, b) => Number(a) + Number(b), numbers.shift());
+    console.log(equalNumbers);
+    console.log(sum);
+}
+
+checkNumbers(2222222);
