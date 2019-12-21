8bfc8b29ee2c28ebc6f2d263e12792d1dbef5480
radomirKrastev <radomirkostantinov@gmail.com>
2019-12-21 21:09:48 +0200

    objects to JSON

---
+++ Objects/towns-to-json.js
@@ -0,0 +1,35 @@
+//@ts-check
+
+function formatDataToJson(arrOfStrings) {
+
+    const parseIfNumber = x => isNaN(x) ? x : Math.round(x * 100) / 100;
+
+    function splitString(str) {
+        return str
+        .split("|")
+        .filter(x => x !== "")
+        .map(x => x.trim())
+        .map(x => parseIfNumber(x));
+    }
+
+    let categories = splitString(arrOfStrings[0]);
+    
+    let objects = arrOfStrings
+    .slice(1)
+    .map(splitString)
+    .map(x => x.reduce((obj, field, i) => {
+        obj[categories[i++]] = field;
+        return obj;
+    }, {}));
+
+    objects.forEach(element => {
+        
+    });
+
+    console.log(JSON.stringify(objects));
+}
+
+console.log(formatDataToJson(['| Town | Latitude | Longitude |',
+'| Sofia | 42.696552 | 23.32601 |',
+'| Beijing | 39.913818 | 116.363625 |']
+));
