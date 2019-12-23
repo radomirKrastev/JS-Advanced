5c52f092a3e8dd5c309a74adc3d69103be3cac95
radomirKrastev <radomirkostantinov@gmail.com>
2019-12-23 16:17:22 +0200

    towns income to json

---
+++ Objects/sum-by-town.js
@@ -0,0 +1,35 @@
+function sumTownsIncome(data) {
+
+    let townsIncome = {};
+
+    for (i = 0; i < data.length; i += 2){
+        let name = data[i];
+        let income = +data[i + 1];
+
+        if(!townsIncome[name]){
+            townsIncome[name] = 0;
+        }
+
+        townsIncome[name] += income;
+    }
+}
+
+sumTownsIncome(['Sofia',
+'20',
+'Varna',
+'3',
+'Sofia',
+'5',
+'Varna',
+'4']
+);
+
+sumTownsIncome(['Sofia',
+'20',
+'Varna',
+'3',
+'sofia',
+'5',
+'varna',
+'4']
+);
