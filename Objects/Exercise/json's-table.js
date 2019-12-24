5323d63618d3287aaff3610582fe053968e632da
radomirKrastev <radomirkostantinov@gmail.com>
2019-12-24 12:59:08 +0200

    JSON to HTML table

--- Objects/Exercise/json's-table.js
+++ Objects/Exercise/json's-table.js
@@ -1,9 +1,21 @@
 function jsonToHtmlTable(data) {
-    let tableContent = JSON.parse(data);
     
-    for(let i = 0; i < data.length; i++){
+    let table = "<table>";
 
+    for(let i = 0; i < data.length; i++){
+        let obj = JSON.parse(data[i]);
+        let keys = Object.getOwnPropertyNames(obj);
+        table += "\n" + "	<tr>";
+
+        for (let key of keys){
+            table += "\n" + `		<td>${obj[key]}</td>`;
+        }
+
+        table += "\n" + "	</tr>";
     }
+
+    table += "\n" + "</table>";
+    console.log(table);
 }
 
 jsonToHtmlTable(['{"name":"Pesho","position":"Promenliva","salary":100000}',
