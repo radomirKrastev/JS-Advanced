2add7d32e15f964428bbf99ac43431c27885f3fe
radomirKrastev <radomirkostantinov@gmail.com>
2019-12-30 19:50:18 +0200

    add simple problem

---
+++ Arrays/Exercise/sort-by-two-criteria.js
@@ -0,0 +1,23 @@
+function sort(arr) {
+    return arr.sort(function(a, b){
+        return (a.length - b.length) || (a.localeCompare(b));
+    }).join("\r\n");
+}
+
+console.log(sort(['alpha', 
+'beta', 
+'gamma']
+));
+
+console.log(sort(['Isacc', 
+'Theodor', 
+'Jack', 
+'Harrison', 
+'George']
+));
+
+console.log(sort(['test', 
+'Deny', 
+'omen', 
+'Default']
+));
