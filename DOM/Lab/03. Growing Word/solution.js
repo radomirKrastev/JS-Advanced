ccb1671e574918bdcc2ba305a5693e648f020c44
radomirKrastev <radomirkostantinov@gmail.com>
2020-01-08 16:00:28 +0200

    implement basic functionality

--- DOM/Lab/03. Growing Word/solution.js
+++ DOM/Lab/03. Growing Word/solution.js
@@ -1,4 +1,18 @@
 function growingWord() {
+  let colorsMap = ["blue", "green", "red"];
 
-  //TODO...
+  let growingWord = document
+  .getElementById("exercise")
+  .getElementsByTagName("p")[0];
+
+  if (growingWord !== null) {    
+    let fontSize = parseFloat(growingWord.style.fontSize.slice(0, -2) * 2);
+    growingWord.style.fontSize = (fontSize === 0 ? 2 : fontSize) + "px";
+    console.log(growingWord.style.fontSize);
+
+    let color = growingWord.style.color;
+    let nextColor = colorsMap.indexOf(color) + 1;
+
+    growingWord.style.color = nextColor > (colorsMap.length - 1) ? colorsMap[0] : colorsMap[nextColor];
+  }
 }
