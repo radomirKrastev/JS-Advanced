33394e4a3128af52b8494bc3f86c16bcba1dcafd
radomirKrastev <radomirkostantinov@gmail.com>
2020-01-08 12:11:43 +0200

    format text basic functionality with DOM

--- DOM/Lab/02. Format the Text/solution.js
+++ DOM/Lab/02. Format the Text/solution.js
@@ -1,3 +1,14 @@
 function solve() {
-  //TODO...
+  let input = document.getElementById("input");
+  let allSentences = input.innerHTML.split(".").filter(x => x !== "");
+
+  let output = document.getElementById("output");
+
+  if (output !== null && input !== null) {
+    for (let s = 0; s < allSentences.length; s += 3){    
+      let paragraph = document.createElement("p");
+      paragraph.innerHTML = allSentences.slice(s, s + 3).join(". ") + ".";
+      output.appendChild(paragraph);
+    }
+  }  
 }
