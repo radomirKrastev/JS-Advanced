1c63c60a57913cc43a8ceeb1b47d99919a140a15
radomirKrastev <radomirkostantinov@gmail.com>
2020-01-08 21:51:41 +0200

    use css to attach listener

--- DOM/Lab/04. Visited Websites/solution.js
+++ DOM/Lab/04. Visited Websites/solution.js
@@ -1,4 +1,21 @@
-function solve() {
+function solve() {  
+  const template = x => `visited ${x} times`;
 
-  //TODO...
+  let links = {};
+
+  let collectionOfLinks = document.getElementsByClassName("link-1");
+
+  for (let i = 0; i < collectionOfLinks.length; i++) {
+    let visits = Number(collectionOfLinks[i].children[1].textContent.trim().split(" ")[1]);
+    links[collectionOfLinks[i].children[0].textContent.trim()] = visits;
+    console.log(links);
+  }
+
+  document.addEventListener("click", (e) => {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.nodeName === "A"){
+      e.target.nextElementSibling.innerHTML = template(++links[e.target.textContent.trim()]);
+    }    
+  }); 
 }
