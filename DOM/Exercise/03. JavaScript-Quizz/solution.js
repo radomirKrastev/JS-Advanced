7573c0428bd904e63d3489158593c4a382c95a48
radomirKrastev <radomirkostantinov@gmail.com>
2020-01-09 17:14:44 +0200

    quizz

--- DOM/Exercise/03. JavaScript-Quizz/solution.js
+++ DOM/Exercise/03. JavaScript-Quizz/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-  //TODO...
+
+  let rightAnswers = ["onclick", "JSON.stringify()", "A programming API for HTML and XML documents"];
+  let points = 0;
+  let sections = [...document.getElementsByTagName("section")];
+
+  const validateResult = (quizIndex) => {
+    if (quizIndex.target.textContent === rightAnswers.shift()) {
+      points++;
+    }
+
+    if (sections.length > 1) {
+      sections.shift().style.display = "none";
+      sections[0].style.display = "block";
+    } else {
+      sections.shift().style.display = "none";
+      document.querySelector(".results-inner").parentNode.style.display = "block";
+
+      document.getElementsByClassName('results-inner')[0].firstElementChild.textContent =
+        points === 3 ? 'You are recognized as top JavaScript fan!' 
+        : `You have ${points} right answers`;
+    }
+  }
+
+  [...document.querySelectorAll('[data-quizIndex]')]
+    .forEach(x => x.getElementsByTagName("p")[0]
+      .addEventListener("click", validateResult));
 }
