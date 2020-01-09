ff895972f9a190dac33061f650ecb5382bdad654
radomirKrastev <radomirkostantinov@gmail.com>
2020-01-09 14:34:15 +0200

    number convertor

--- DOM/Exercise/02. Number-Convertor/solution.js
+++ DOM/Exercise/02. Number-Convertor/solution.js
@@ -1,4 +1,25 @@
 function solve() {
+    let BinaryOprion = document.createElement("option");
+    let HexadeicmalOprion = document.createElement("option");
 
-    //TODO...
+    BinaryOprion.text = "binary";
+    HexadeicmalOprion.text = "hexadeicmal";
+
+    let menu = document.querySelector('#selectMenuTo');
+    menu.appendChild(BinaryOprion);
+    menu.appendChild(HexadeicmalOprion);
+
+    const convert = () => {
+        let selected = menu.options[menu.selectedIndex];
+        let input = document.querySelector("#input");
+
+        let convertedValue = selected.text === "binary" 
+        ?  Number(input.value).toString(2) 
+        : Number(input.value).toString(16);
+        
+        let result = document.querySelector("#result");
+        result.value = convertedValue.toString().toUpperCase();
+    };
+    
+    document.getElementsByTagName("button")[0].addEventListener("click", convert);
 }
