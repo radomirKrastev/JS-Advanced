f1ef83e41ab68f006d0f7512c15bce05d5e06d94
radomirKrastev <radomirkostantinov@gmail.com>
2020-01-10 12:56:17 +0200

    order the names

--- DOM/Exercise/05. Order-the-Names/solution.js
+++ DOM/Exercise/05. Order-the-Names/solution.js
@@ -1,4 +1,24 @@
 function solve() {
+    let button = document.querySelector("#exercise button");
+    
+    if(button !== null) {
+        const addName = () => {
+            let nameTable = [...document.querySelectorAll("ol li")];
+            
+            if (nameTable !== null) {
+                let name = document.querySelector("#exercise input");
+                name.value = name.value.charAt(0).toUpperCase() + name.value.substring(1).toLowerCase();
+                let firstLetterASCII = name.value[0].charCodeAt(0);
+                let index = firstLetterASCII - 65;
+    
+                if (index >= 0) {
+                    nameTable[index].textContent = nameTable[index].textContent.length === 0 
+                    ? nameTable[index].textContent = name.value
+                    : nameTable[index].textContent += `, ${name.value}`;
+                }
+            }
+        };
 
-    //TODO...
+        button.addEventListener("click", addName);
+    }
 }
