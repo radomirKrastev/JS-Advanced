86c645c764331517cc4a0234214cc0e95a751624
radomirKrastev <radomirkostantinov@gmail.com>
2020-01-10 18:24:23 +0200

    update

--- DOM/Exercise/06. Table-Search-Engine/solution.js
+++ DOM/Exercise/06. Table-Search-Engine/solution.js
@@ -1,3 +1,39 @@
 function solve() {
-   //TODO...
+   let button = document.querySelector("td button");
+
+   function isAMatch (row, input) {
+      for(let studentData of [...row.querySelectorAll("td")])
+      {
+         if (studentData.textContent.includes(input)){
+            return true;
+         }
+      }
+
+      return false;
+   }
+
+   function clearClassList (classList) {
+      while (classList.length > 0) {
+         console.log("?");
+         classList.remove(classList.item(0));
+      }
+   }
+
+   if (button !== null) {
+      const search = () => {
+         let input = document.querySelector("td input");
+         let rows = [...document.querySelectorAll("tbody tr")]; 
+         rows.map(x => clearClassList(x.classList));
+   
+         if (input !== null) {
+            rows
+            .filter(x => isAMatch(x, input.value))
+            .map(x => x.classList.add("select"));
+         }
+   
+         input.value = ""; 
+      };   
+      
+      button.addEventListener("click", search);
+   }
 }
--- DOM/Exercise/07. Shopping-Cart/solution.js
+++ DOM/Exercise/07. Shopping-Cart/solution.js
@@ -1,3 +1,33 @@
 function solve() {
-   //TODO...
+   // let totalPrice = 0;
+   // let set = new Set();
+
+    function addProductsToCart(x) {
+      console.log(x);
+      console.log(x.target);
+      //let product = x.target.parentElement.parentElement;
+      //let name = product.querySelector("product-title").textContent;
+      //let price = Number(product.querySelector("product-line-price").textContent);
+
+   //    console.log(name);
+   //    console.log(price);
+
+   //    set.add(name);
+   //    totalPrice += price;
+   //    console.log(name);
+   //    console.log(price);
+
+   //    let messageArea = product.parentElement;
+   //    messageArea.textContent = `Added ${name} for ${money} to the cart.\n`;
+   //    console.log("-----------------------");
+   }
+
+   // console.log(document.querySelectorAll(".add-product")[0]);
+   // console.log(document.querySelectorAll(".add-product")[0].closest(".product-line-price"));
+   // console.log([...document.querySelectorAll(".add-product")]);
+
+   let buttons = [...document.querySelectorAll(".add-product")].forEach(x => x.addEventListener("click", addProductsToCart(x)));
+   //buttons.forEach(x => x.addEventListener("click", addProductsToCart(x)));
+
+
 }
