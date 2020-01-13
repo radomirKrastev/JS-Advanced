88735174435d8ef7223df8e9234f1290cd8ffe3b
radomirKrastev <radomirkostantinov@gmail.com>
2020-01-13 11:16:46 +0200

    update

--- DOM/Exercise/09. Cards/solution.js
+++ DOM/Exercise/09. Cards/solution.js
@@ -1,3 +1,48 @@
 function solve() {
-   // TODO...
+   let cards = [...document.querySelectorAll("#player1Div img, #player2Div img")];
+
+   let findGreaterCard = function (card) {
+      console.log(card);
+      let result = document.querySelector("#result");
+      if (card.parentElement.id === "player1Div") {
+         result.firstElementChild.textContent = card.name;
+      } else {
+         result.lastElementChild.textContent = card.name;
+      }
+
+      card.src = "images/whiteCard.jpg";
+
+      let topCardValue = result.firstElementChild.textContent;
+      let bottomCardValue = result.lastElementChild.textContent;
+      let winnerValue = "";
+
+      if (topCardValue !== "" && bottomCardValue !== "") {
+         winnerValue = Math.max(Number(topCardValue), Number(bottomCardValue));
+         console.log(winnerValue);
+
+         let winnerCard;
+         let looserCard;
+
+         if (winnerValue == topCardValue) {
+            winnerCard = document.querySelector(`#player1Div img[name="${topCardValue}"]`);
+            looserCard = document.querySelector(`#player2Div img[name="${bottomCardValue}"]`);
+         } else {
+            winnerCard = document.querySelector(`#player2Div img[name="${bottomCardValue}"]`);
+            looserCard = document.querySelector(`#player1Div img[name="${topCardValue}"]`);
+         }
+
+         winnerCard.style.border = "2px solid green";
+         looserCard.style.border = "2px solid red";
+
+         let history = document.querySelector("#history");
+         
+         history.textContent += `[${topCardValue} vs ${bottomCardValue}] `;
+
+         result.firstElementChild.textContent = "";
+         result.lastElementChild.textContent = "";         
+      }
+   };
+
+   console.log(cards);
+   cards.map(x => x.addEventListener("click", findGreaterCard.bind(null, x)));
 }
--- DOM/Exercise/10. Sudomu/solution.js
+++ DOM/Exercise/10. Sudomu/solution.js
@@ -1,4 +1,62 @@
 function solve() {
+    let rows = [...document.querySelectorAll("tbody tr")];
+    let rowCells = rows.map(x => [...x.querySelectorAll("td")]);
+    let quickCheck = document.querySelectorAll("button")[0];
 
-    //TODO...
+    let checkForUniqueValuesInLimits = function (arr) {
+        arr.filter(x => x >= 1 && x <= 3);
+
+        for (let row of arr) {
+            if (new Set(row.slice().filter(x => x >= 1 && x <= 3)).size !== arr.length){
+                return false;
+            }            
+        }
+        
+        return true;
+    };
+
+    let check = function () {
+        let rowsAreValid = true;
+        //check row values
+
+        let filledRows = rowCells
+        .map(r => r.map(c => c = Number(c.querySelector("input").value)));
+        //.filter(x => x >= 1 && x <= 3);
+
+        // for (let row of rowCells) {
+        //     // if (row.map(x => Number(x.textContent).reudce((a, b) => a + b, 0)) === 6 
+        //     // && (new Set(row.map(Number(x.textContent)).filter(x => x >= 1 && x <= 3))).size !== array.length)
+        //     console.log(row);
+
+        //     if ((new Set(row
+        //         .map(r => Number(r.querySelector("input").value))
+        //         .filter(x => x >= 1 && x <= 3))).size !== row.length) {
+        //         rowsAreValid = false;
+        //         return;
+        //     }
+        // }
+
+        console.log(rowsAreValid);
+
+        let cols = rowCells.reduce(function (result, r, i) {
+            for (let cell of r) {
+                console.log(Number(cell.querySelector("input").value));
+                result[i].push(Number(cell.querySelector("input").value));
+            }
+
+            return result;
+        }, [[],[],[]]);
+
+        console.log(cols);
+
+        if (checkForUniqueValuesInLimits(filledRows) && checkForUniqueValuesInLimits(cols)){
+            let output = document.querySelector("div[id='check']");
+            output.textContent = "You solve it! Congratulations!";
+            console.log("EVERYTHING IS PERFECT, BRO, NO WORRIES");
+        } else {
+            console.log("YOU FUCKED UP SOMETHING!!!");
+        }
+    };
+
+    quickCheck.addEventListener("click", check);
 }
