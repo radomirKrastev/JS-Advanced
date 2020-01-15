a6b765625536128c54fcbc6b6c141d050e6c0ba5
radomirKrastev <radomirkostantinov@gmail.com>
2020-01-15 22:53:49 +0200

    upload third problem from LAB

--- DOM Manipulation/LAB/03. Delete-From-Table/app.js
+++ DOM Manipulation/LAB/03. Delete-From-Table/app.js
@@ -1,3 +1,26 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const getElement = function(element) {
+        return document.querySelector(element);
+    };
+
+    const getElements = function(elements) {
+        return document.querySelectorAll(elements);
+    };    
+
+    let deleteRow = function(input, rows) {
+        let currentEmailRow = rows.find(r => r.children[1].innerHTML === input);
+
+        if (currentEmailRow){
+            let body = currentEmailRow.parentElement;
+            return body.removeChild(currentEmailRow);
+        }
+
+        let result = getElement("#result");
+        result.innerHTML = "Not found.";
+        return result;
+    }; 
+
+    let rows = [...getElements("tbody tr")];
+    let input = getElement("input").value;
+    deleteRow(input, rows);
 }
