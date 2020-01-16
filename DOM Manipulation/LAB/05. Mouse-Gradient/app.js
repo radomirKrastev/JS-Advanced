1dc7521f8d33d3262935fe2773a41c58d1d7a212
radomirKrastev <radomirkostantinov@gmail.com>
2020-01-16 12:38:36 +0200

    upload fifth problem from LAB

--- DOM Manipulation/LAB/05. Mouse-Gradient/app.js
+++ DOM Manipulation/LAB/05. Mouse-Gradient/app.js
@@ -1,3 +1,14 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const getElement = function (element) {
+        return document.querySelector(element);
+    };
+
+    const showPosition = function(e) {
+        let result = getElement("#result");
+        result.innerHTML = Math.floor((e.offsetX / gradient.clientWidth) * 100) + "%";
+        console.log(result.innerHTML);
+    };
+
+    let gradient = getElement("#gradient");
+    gradient.addEventListener("mousemove", showPosition);
 }
