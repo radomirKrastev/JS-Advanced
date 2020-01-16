5bdd80782b58ce11d634360fae65dc1b6ee9d4d9
radomirKrastev <radomirkostantinov@gmail.com>
2020-01-16 17:35:26 +0200

    upload first and second problem from LAB

--- DOM Manipulation/Exercise/01. Subtraction/subtract.js
+++ DOM Manipulation/Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,11 @@
 function subtract() {
-    console.log('TODO:...');
+    let getElement = function(element) {
+        return document.querySelector(element);
+    };
+
+    let firstNumber = getElement("#firstNumber");
+    let secondNumber = getElement("#secondNumber");
+    let result = getElement("#result");
+
+    result.innerHTML = Number(firstNumber.value) - Number(secondNumber.value);
 }
--- DOM Manipulation/Exercise/02. Fill-Dropdown/dropdown.js
+++ DOM Manipulation/Exercise/02. Fill-Dropdown/dropdown.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+    let fields = [...document.querySelectorAll("#newItemText, #newItemValue")];
+    let option = document.createElement("option");
+    let menu = document.querySelector("#menu");
+
+    option.textContent = fields[0].value;
+    option.value = fields[1].value;
+    menu.appendChild(option);
+
+    fields[0].value = "";
+    fields[1].value = "";
 }
