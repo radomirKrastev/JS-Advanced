50e86a7fd911e54e37e19ae350f88a1095e3f20e
radomirKrastev <radomirkostantinov@gmail.com>
2020-01-16 13:37:13 +0200

    upload sixth problem from LAB

--- DOM Manipulation/LAB/06. Highlight-Active/app.js
+++ DOM Manipulation/LAB/06. Highlight-Active/app.js
@@ -1,3 +1,15 @@
 function focus() {
-    console.log('TODO:...');
+    const getElementsAsList = function(elements) {
+        return [...document.querySelectorAll(elements)];
+    };
+
+    let focusHandler = function(e) {
+        getElementsAsList("input[type='text']")
+        .map(x => x.parentElement.className='blurred');
+
+        e.target.parentElement.className = 'focused';
+    };
+
+    getElementsAsList("input[type='text']")
+    .forEach(x => x.addEventListener("focus", focusHandler));
 }
