38b820c94a50b896eebfc25c9dd2003a15e52377
radomirKrastev <radomirkostantinov@gmail.com>
2020-01-16 14:06:43 +0200

    upload seventh problem from LAB

--- DOM Manipulation/LAB/07. Dynamic-Validation/app.js
+++ DOM Manipulation/LAB/07. Dynamic-Validation/app.js
@@ -1,3 +1,18 @@
 function validate() {
-    console.log('TODO:...');
+    let validateEmail = function (e) {
+        let input = e.target;
+        let pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+        if(!pattern.test(input.value)){
+            input.className = 'error';
+        } else {
+            input.className = '';
+        }
+    };
+
+    let validationHandler = function(e) {
+        validateEmail(e);
+    };
+
+    document.querySelector("input[type='text']").addEventListener("change", validationHandler);
 }
