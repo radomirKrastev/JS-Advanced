a6b657ca01e4e761730406fe67c9eb561deff1b3
radomirKrastev <radomirkostantinov@gmail.com>
2020-01-17 13:04:54 +0200

    upload fourth problem from Exercise

--- DOM Manipulation/Exercise/04. Sections/script.js
+++ DOM Manipulation/Exercise/04. Sections/script.js
@@ -1,3 +1,24 @@
 function create(words) {
-   console.log('TODO:...');
+   let handlerShowText = function(paragraph) {
+      console.log(paragraph);
+      paragraph.style.display = 'block';
+   };
+
+   let attachElements = function (text) {
+      let div = document.createElement("div");
+      let paragraph = document.createElement("p");
+
+      paragraph.innerHTML = text;
+      paragraph.style.display = 'none';
+      div.appendChild(paragraph);
+      div.addEventListener("click", handlerShowText.bind(null, paragraph));
+      
+      return content.appendChild(div);
+   };
+
+   let content = document.querySelector("#content");
+
+   for (let text of words) {
+      attachElements(text);
+   }
 }
