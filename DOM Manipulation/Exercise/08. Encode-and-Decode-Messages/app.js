43c793d2a19e7f69474ceddd810dc6edbbc7e3a7
radomirKrastev <radomirkostantinov@gmail.com>
2020-01-17 21:29:05 +0200

    upload distanceConverter from Exercise

--- DOM Manipulation/Exercise/08. Encode-and-Decode-Messages/app.js
+++ DOM Manipulation/Exercise/08. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,47 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let buttons = [...document.querySelectorAll("button")];
+    let receiverText = document.querySelector("textarea[placeholder='No messages...']");
+
+    let encodeText = function(e) {
+        let parent = e.target.parentElement;
+        let text = parent.querySelector("textarea");
+
+        if (text.value && text.value !== "") {
+            let encodedText = "";
+
+            for (let i = 0; i < text.value.length; i++){
+                let asciiEncoded = text.value.charCodeAt(i) + 1;
+                let enCodedLetter = String.fromCharCode(asciiEncoded);
+                encodedText += enCodedLetter;
+            }
+
+            receiverText.value = encodedText;
+            text.value = "";
+        }
+    };
+
+    let decodeText = function() {
+        if(receiverText.value && receiverText.value !== "") {
+            let decodedText = "";
+
+            for (let i = 0; i < receiverText.value.length; i++){
+                let asciiDecoded = receiverText.value.charCodeAt(i) - 1;
+                let deCodedLetter = String.fromCharCode(asciiDecoded);
+                decodedText += deCodedLetter;
+            }
+
+            receiverText.value = decodedText;
+        }
+    };
+
+    let encodeHandler = function(e) {
+      encodeText(e);  
+    };
+    
+    let decodeHandler = function() {
+        decodeText();
+    };
+
+    let encodeButton = buttons[0].addEventListener("click", encodeHandler);
+    let decodeButton = buttons[1].addEventListener("click", decodeHandler);
 }
--- DOM Manipulation/Exercise/09. Distance-Converter/distanceConverter.js
+++ DOM Manipulation/Exercise/09. Distance-Converter/distanceConverter.js
@@ -1,3 +1,35 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    let meterRatio = {
+        km: 1000,
+        m: 1,
+        cm: 0.01,
+        mm: 0.001,
+        mi: 1609.34,
+        yrd: 0.9144,
+        ft: 0.3048,
+        in: 0.0254
+    };
+
+    let getElement = function(element) {
+        return document.querySelector(element);
+    };
+
+    let convertDistance = function () {
+        let inputDistance = getElement("#inputDistance");
+        let inputUnits =  getElement("#inputUnits");
+        let selectedInputMetric = inputUnits.options[inputUnits.selectedIndex].value;
+        let inputInMeters = inputDistance.value * meterRatio[selectedInputMetric];
+                
+        let outputUnits =  getElement("#outputUnits");
+        let selectedOutputMetric = outputUnits.options[outputUnits.selectedIndex].value;
+        let outputDistance = getElement("#outputDistance");
+        return outputDistance.value = inputInMeters / meterRatio[selectedOutputMetric];
+    };
+
+    let convertHandler = function() {
+        convertDistance();
+    };
+
+    let convertButton = getElement("#convert");
+    convertButton.addEventListener("click", convertHandler);
 }
