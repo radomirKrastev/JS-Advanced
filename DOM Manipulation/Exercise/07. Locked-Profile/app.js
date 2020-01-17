7530e0e10c4ae646655904489094f7664ac3956c
radomirKrastev <radomirkostantinov@gmail.com>
2020-01-17 16:43:17 +0200

    upload lockedProfile from Exercise

--- DOM Manipulation/Exercise/07. Locked-Profile/app.js
+++ DOM Manipulation/Exercise/07. Locked-Profile/app.js
@@ -1,3 +1,24 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let hideShowInfo = function(hiddenFields, e) {
+        if (e.target.innerHTML === "Show more") {                        
+        hiddenFields.style.display = 'block';
+        e.target.innerHTML = "Hide it";
+        } else {
+            hiddenFields.style.display = 'none';
+            e.target.innerHTML = "Show more";
+        }
+    };
+    
+    let infoHandler = function (e) {
+        let parent = e.target.parentElement;
+        let lockRadio = parent.querySelector("input[value='lock']");
+
+        if (lockRadio.checked === false) {
+            let hiddenFields = parent.querySelector("input[type='email']").parentElement;
+            hideShowInfo(hiddenFields, e);            
+        }
+    };
+
+    let buttons = [...document.querySelectorAll("button")];
+    buttons.map(x => x.addEventListener("click", infoHandler));
 }
