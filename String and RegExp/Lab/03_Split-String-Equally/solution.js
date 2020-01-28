ef30da16df40445eb3948c41036dae60dd4679fe
radomirKrastev <radomirkostantinov@gmail.com>
2020-01-28 15:43:10 +0200

    upload third lab problem

--- String and RegExp/Lab/03_Split-String-Equally/solution.js
+++ String and RegExp/Lab/03_Split-String-Equally/solution.js
@@ -1,3 +1,17 @@
 function solve() {
-    //TODO...
+  let text = document.querySelector("#text").value;
+  let number = parseInt(document.querySelector("#number").value);
+  let result = document.querySelector("#result");
+  let pattern = new RegExp(`.{1,${number}}`, "g");
+
+  let output = text.match(pattern);
+
+  while (output[output.length - 1].length < number) {
+    output[output.length - 1] += text.slice(
+      0,
+      number - output[output.length - 1].length
+    );
+  }
+
+  result.innerHTML = output.join(" ");
 }
