d30cd13f15a8cde1c660f9423dae3213089bae9c
radomirKrastev <radomirkostantinov@gmail.com>
2020-01-28 11:44:22 +0200

    upload first lab problem refactored

--- String and RegExp/Lab/01_Pascal-or-Camel-Case/solution.js
+++ String and RegExp/Lab/01_Pascal-or-Camel-Case/solution.js
@@ -1,3 +1,42 @@
 function solve() {
-  //TODO...
+  let getElement = function(element) {
+    return document.querySelector(element);
+  };
+
+  let turnFirstCharToUpper = function(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  };
+
+  let types = {
+    "Pascal Case": function(str) {
+      return str.map((x) => turnFirstCharToUpper(x)).join("");
+    },
+    "Camel Case": function(str) {
+      return `${str[0]}${str
+        .slice(1, str.length + 1)
+        .map((x) => turnFirstCharToUpper(x))
+        .join("")}`;
+    }
+  };
+
+  let transformText = function(text, namingConvention, result) {
+    if (text.value.length > 0 && namingConvention.value.length > 0) {
+      let output = "";
+      let words = text.value.split(" ").map((x) => x.toLowerCase());
+
+      if (typeof types[namingConvention.value] === "undefined") {
+        output = "Error!";
+      } else {
+        output = types[namingConvention.value](words);
+      }
+
+      result.innerHTML = output;
+    }
+  };
+
+  let text = getElement("#text");
+  let namingConvention = getElement("#naming-convention");
+  let result = getElement("#result");
+
+  transformText(text, namingConvention, result);
 }
--- String and RegExp/Lab/02_Find-Ascii-Equivalent/solution.js
+++ String and RegExp/Lab/02_Find-Ascii-Equivalent/solution.js
@@ -1,4 +1,29 @@
 function solve() {
+  let text = document.querySelector("#text");
+  let result = document.querySelector("#result");
 
-  //TODO...
+  let getChar = function(x) {
+    return String.fromCharCode(Number(x));
+  };
+
+  let getCode = function(symbol) {
+    return symbol.charCodeAt(0);
+  };
+
+  let parts = text.value.split(" ");
+
+  let word = parts
+    .filter((x) => !isNaN(x))
+    .map((x) => getChar(x))
+    .join("");
+
+  let asciiCodes = parts
+    .filter((x) => isNaN(x))
+    .map((str) =>
+      str
+        .split("")
+        .map((x) => getCode(x))
+        .join(" ")
+    )
+    .join("\n");
 }
