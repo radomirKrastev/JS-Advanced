e49ac7c118023dd770bd229fcb2599623e95a666
radomirKrastev <radomirkostantinov@gmail.com>
2020-01-28 16:13:47 +0200

    upload fourth lab problem

--- String and RegExp/Lab/04_Replace-a-Certain-Word/solution.js
+++ String and RegExp/Lab/04_Replace-a-Certain-Word/solution.js
@@ -1,4 +1,16 @@
 function solve() {
+  let word = document.querySelector("#word").value;
+  let text = document.querySelector("#text").value;
+  let result = document.querySelector("#result");
 
-    //TODO...
+  let strings = text.match(/(?<=")[^",]+(?=")/gi);
+  let wordToBeReplaced = strings[0].split(" ")[2];
+
+  let replacePattern = new RegExp(wordToBeReplaced, "gi");
+
+  for (let line of strings) {
+    let p = document.createElement("p");
+    p.textContent = line.replace(replacePattern, word);
+    result.appendChild(p);
+  }
 }
