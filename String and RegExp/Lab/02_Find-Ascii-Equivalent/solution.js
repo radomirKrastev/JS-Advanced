911bb6cd1ef2a1e55d042c957658fc489ce5145d
radomirKrastev <radomirkostantinov@gmail.com>
2020-01-28 13:04:22 +0200

    upload second lab problem

--- String and RegExp/Lab/02_Find-Ascii-Equivalent/solution.js
+++ String and RegExp/Lab/02_Find-Ascii-Equivalent/solution.js
@@ -2,6 +2,12 @@ function solve() {
   let text = document.querySelector("#text");
   let result = document.querySelector("#result");
 
+  let appendToResult = function(text) {
+    let p = document.createElement("p");
+    p.textContent = text;
+    result.appendChild(p);
+  };
+
   let getChar = function(x) {
     return String.fromCharCode(Number(x));
   };
@@ -10,20 +16,30 @@ function solve() {
     return symbol.charCodeAt(0);
   };
 
-  let parts = text.value.split(" ");
+  let getOutputObj = function() {
+    let parts = text.value.split(" ");
+    return (obj = {
+      word: parts
+        .filter((x) => !isNaN(x))
+        .map((x) => getChar(x))
+        .join(""),
 
-  let word = parts
-    .filter((x) => !isNaN(x))
-    .map((x) => getChar(x))
-    .join("");
+      asciiCodes: parts
+        .filter((x) => isNaN(x))
+        .map((str) =>
+          str
+            .split("")
+            .map((x) => getCode(x))
+            .join(" ")
+        )
+    });
+  };
 
-  let asciiCodes = parts
-    .filter((x) => isNaN(x))
-    .map((str) =>
-      str
-        .split("")
-        .map((x) => getCode(x))
-        .join(" ")
-    )
-    .join("\n");
+  let output = getOutputObj();
+
+  for (let line of output.asciiCodes) {
+    appendToResult(`${line}\n`);
+  }
+
+  appendToResult(`${output.word}`);
 }
