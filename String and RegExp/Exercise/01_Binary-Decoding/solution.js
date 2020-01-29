630d66b88774f4d9e87d1dcb2c7e716d52658e82
radomirKrastev <radomirkostantinov@gmail.com>
2020-01-29 15:34:36 +0200

    upload first exercise problem

--- String and RegExp/Exercise/01_Binary-Decoding/solution.js
+++ String and RegExp/Exercise/01_Binary-Decoding/solution.js
@@ -1,3 +1,31 @@
 function solve() {
-	//TODO...
+  const input = document.querySelector("#input").value;
+  const result = document.querySelector("#resultOutput");
+  let binaryToDecimal = function(input) {
+    let sum = input;
+
+    while (sum.length > 1) {
+      sum = sum
+        .split("")
+        .reduce(function(result, i) {
+          result += Number(i);
+          return result;
+        }, 0)
+        .toString();
+    }
+
+    let trimmedCode = input.substring(sum, input.length - sum);
+
+    console.log(trimmedCode);
+    console.log(trimmedCode.split(/([0-1]{8})/));
+    console.log(trimmedCode.split(/([\d]{8})/));
+    return trimmedCode
+      .split(/([\d]{8})/)
+      .map((x) => parseInt(x, 2))
+      .map((x) => String.fromCharCode(x))
+      .filter((x) => x.match(/[A-Za-z\s]/g))
+      .join("");
+  };
+
+  result.innerHTML += binaryToDecimal(input);
 }
