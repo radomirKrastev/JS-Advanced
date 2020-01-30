fb97b9b624e003754301e5b74e9540514f97db7d
radomirKrastev <radomirkostantinov@gmail.com>
2020-01-30 13:40:44 +0200

    upload second exercise problem

--- String and RegExp/Exercise/02_Expedition-Problems/solution.js
+++ String and RegExp/Exercise/02_Expedition-Problems/solution.js
@@ -1,4 +1,47 @@
 function solve() {
+  let cryptedText = document.querySelector("#text").value;
+  let keyWord = document.querySelector("#string").value;
+  let result = document.querySelector("#result");
 
-    //TODO...
+  let createElement = function(elmt, text) {
+    let element = document.createElement(elmt);
+    element.textContent = text;
+    return element;
+  };
+
+  let fillResult = function(result, text) {
+    result.appendChild(createElement("p", text));
+  };
+
+  let getCoordinates = function(text) {
+    const pattern = /(north|east)[\s\S]*?([0-9]{2})[^,]*?,[^,]*?([0-9]{6})/gim;
+    let north;
+    let east;
+
+    while ((valid = pattern.exec(text)) !== null) {
+      const northOrEast = valid[1];
+
+      if (northOrEast.toLowerCase() === "north") {
+        north = `${valid[2]}.${valid[3]} N`;
+      } else if (northOrEast.toLowerCase() === "east") {
+        east = `${valid[2]}.${valid[3]} E`;
+      }
+    }
+
+    return [north, east];
+  };
+
+  let getMessage = function(key, text) {
+    let pattern = new RegExp(`(?<=${key}).+(?=${key})`, "im");
+    return pattern.exec(text)[0];
+  };
+
+  let output = [];
+  let coordinates = getCoordinates(cryptedText);
+
+  output.push(coordinates[0]);
+  output.push(coordinates[1]);
+  output.push(`Message: ${getMessage(keyWord, cryptedText)}`);
+
+  output.forEach((x) => fillResult(result, x));
 }
